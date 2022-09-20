f24cef7c2212f06ea41e8a4fbb8d65e5335a6a94
Sonsuz Dongu <sonsuz_dongu_youtube@hotmail.com>
2022-09-20 22:10:13 +0300

    veriables
    Java Camp

---
+++ veriables/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String mesaj = "Ogrenci Sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Ogrenci Sayim : " + ogrenciSayisi);
+    }
+}
