333e7234a4cb68112c2e877688c879fa445ef31e
Sonsuz Dongu <sonsuz_dongu_youtube@hotmail.com>
2022-09-20 22:23:30 +0300

    Conditionals
    Java Camp

---
+++ conditionals/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int sayi = 15;
+     if(sayi < 10){
+         System.out.println("Sayi 10'dan kucuktur");
+     }
+     else if(sayi == 10){
+         System.out.println("Sayi 10'a esittir");
+     }
+     else{
+         System.out.println("Sayi 10'dan buyuktur");
+     }
+
+
+    }
+
+    }
