37c8a69253ee533ccd2902bb28ea87c54b05ca4c
Sonsuz Dongu <sonsuz_dongu_youtube@hotmail.com>
2022-09-20 22:15:33 +0300

    Data Types
    Java Camp

---
+++ dataTypes/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int sayi = 12;
+    double sayi2 = 12.5;
+    char karakter = 'M';
+    boolean dogruMu = false;
+
+    }
+}
