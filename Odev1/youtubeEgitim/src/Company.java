98e809005fd0c71ff07859c3181e4348131f9719
meryemertrk <111062589+meryemertrk@users.noreply.github.com>
2022-10-08 01:27:21 +0300

    3.Hafta
    3.Hafta

---
+++ Odev1/youtubeEgitim/src/Company.java
@@ -0,0 +1,5 @@
+public class Company extends Customer{
+    public String taxNumber;
+    public String companyName;
+
+}
---
+++ Odev1/youtubeEgitim/src/TeacherCreditManager.java
@@ -0,0 +1,8 @@
+public class TeacherCreditManager extends BaseCreditManager implements ICreditManager{
+    @Override
+    public void calculate() {
+        System.out.println("Öğretmen kredisi hesaplandı");
+    }
+
+
+}
