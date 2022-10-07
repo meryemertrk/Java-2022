4f0036a30351b907cdb2f4e5e855f4c766762cf4
meryemertrk <111062589+meryemertrk@users.noreply.github.com>
2022-10-08 01:32:56 +0300

    Delete 3.Hafta/Odev1/youtubeEgitim directory

--- 3.Hafta/Odev1/youtubeEgitim/src/ICreditManager.java
+++
@@ -1,4 +0,0 @@
-public interface ICreditManager {
-    void calculate();
-    void save();
-}
--- 3.Hafta/Odev1/youtubeEgitim/src/MilitaryCreditManager.java
+++
@@ -1,8 +0,0 @@
-public class MilitaryCreditManager extends BaseCreditManager implements ICreditManager {
-    @Override
-    public void calculate() {
-        System.out.println("Asker kredisi hesaplandı");
-    }
-
-
-}
