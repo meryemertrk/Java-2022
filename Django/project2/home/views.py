66d18d30fdff4eac3540c39f84d96698df5d8916
Meryem Ertürk <111062589+meryemertrk@users.noreply.github.com>
2022-10-09 15:12:02 +0300

    Delete Django/project2 directory

--- Django/project2/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    text = "Hello Django"
-    return HttpResponse("You're looking at question %s." % text)
-
